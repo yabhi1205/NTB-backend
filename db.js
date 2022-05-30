1b16aa083b4e176f42ffee9accf234ca18ad3110
yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2022-05-30 22:13:10 +0530

    online database

--- db.js
+++ db.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/Users?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 const connectToMongo =()=>{
     mongoose.connect(mongoURI,()=>{
+const mongoURI = "mongodb+srv://abhishek:abhishek123@ntb.etponjw.mongodb.net/?retryWrites=true&w=majority"
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
         console.log("Connected to Server Successfully...")
     })
 }
+
 module.exports = connectToMongo
