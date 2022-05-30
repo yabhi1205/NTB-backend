df8a06152d8ab5e854507d5a35232b7d1ab55b6d
yabhi1205 <83153764+yabhi1205@users.noreply.github.com>
2022-05-30 22:13:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/Users?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-const connectToMongo =()=>{
-    mongoose.connect(mongoURI,()=>{
 const mongoURI = "mongodb+srv://abhishek:abhishek123@ntb.etponjw.mongodb.net/?retryWrites=true&w=majority"
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
